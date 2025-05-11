8f711596096257a45bdfe30e3e1f3ee357cff4a6
JoseLuisGL <89991775+JoseLuisGL@users.noreply.github.com>
2025-05-11 13:16:22 -0700

    Funcion de balas

--- galaga.js
+++ galaga.js
@@ -1,5 +1,6 @@
 let player;
 let playerImg;
+let bullets = []; // Lista de balas
 
 function preload() {
   playerImg = loadImage('nave.png'); // Sprite de jugador -- nave
@@ -11,12 +12,31 @@ function setup() {
 }
 
 function draw() {
-  background(0); //negro
+  background(0); // negro
+
   player.update();
   player.show();
+
+  // mostrar y actu bala
+  for (let i = bullets.length - 1; i >= 0; i--) {
+    bullets[i].update();
+    bullets[i].show();
+
+    // Si bala sale
+    if (bullets[i].offscreen()) {
+      bullets.splice(i, 1);
+    }
+  }
 }
 
-class Player { //propiedades del player
+function keyPressed() {
+  if (key === ' ') { // boton bala
+    // Crear bala bien desde la punta de la nave
+    bullets.push(new Bullet(player.x + player.width / 2, player.y));
+  }
+}
+
+class Player {
   constructor() {
     this.width = 40;
     this.height = 40;
@@ -25,7 +45,7 @@ class Player { //propiedades del player
     this.speed = 5;
   }
 
-  update() { //movimiento
+  update() {
     if (keyIsDown(LEFT_ARROW)) {
       this.x -= this.speed;
     }
@@ -38,5 +58,28 @@ class Player { //propiedades del player
 
   show() {
     image(playerImg, this.x, this.y, this.width, this.height);
+  }
+}
+
+class Bullet {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.speed = 2;
+    this.radius = 5;
+  }
+
+  update() {
+    this.y -= this.speed;
+  }
+
+  show() {
+    fill(255);
+    noStroke();
+    ellipse(this.x, this.y, this.radius * 2);
+  }
+
+  offscreen() {
+    return this.y < 0;
   }
 }
